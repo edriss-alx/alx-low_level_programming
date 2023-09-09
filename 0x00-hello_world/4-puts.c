41240aae177c59f8f40eb55837bb48194875fbb9
edriss bch <edriss@edriss.ch>
2023-09-09 10:41:05 -0700

    add all

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: this function write that sentence on the std out
+ *
+ * Return: always (0) when succeed
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description : this code print that sentence on teh stndr out using printf.
+ *
+ * Return: always (0) when on succeed
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
